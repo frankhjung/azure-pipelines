657d939eb97dad293d0753b6151ac1b789293b2d
Frank Jung <frankhjung@users.noreply.github.com>
2019-06-29 14:04:56 +1000

    added hello world

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Test Azure pipelines for Python applications.
+"""
+
+#
+# MAIN
+#
+if __name__ == '__main__':
+
+    __version__ = '0.1.0'
+    NAME = 'Azure Pipeline'
+    print("Hello %{NAME}!")
